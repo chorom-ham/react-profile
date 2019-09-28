b3e7450ec3b14cb583a7d22b317959c483eba9be
chorom-ham <52379950+chorom-ham@users.noreply.github.com>
2019-09-29 00:19:25 +0900

    미션 코드 완성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
+import Introduction from './Introduction';
 
 class App extends Component {
-  render() {
-    return <div className="App">Hello React!</div>;
+  constructor(props){
+    super(props);
+    this.state = {
+      info:[{key:1, id:1, name:"최수민", univ:"서강", major:"컴퓨터공학", age:"25", emotion:"행복", animal:["사자", "토끼", "뱀"]},
+      {key:2, id:2, name:"이한길", univ:"홍익" , major:"법학", age:"29", emotion:"불행", animal:["펭귄"]}, 
+      {key:3, id:3, name:"김서영", univ:"이화여자", major:"사이버보안학", age:"22", emotion:"불행", animal:["웜", "트로이목마"]}]
+    }
   }
-}
 
+  render() {
+    return <div className="App"> 
+      <Introduction data={this.state.info[0]}></Introduction>
+      <Introduction data={this.state.info[1]}></Introduction>
+      <Introduction data={this.state.info[2]}></Introduction>
+    </div>;
+  }
+
+}
 export default App;
---
+++ src/Introduction.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class Introduction extends Component {
+  render() {
+    var data = this.props.data;
+    var animalData = this.props.data.animal;
+    var animallist = [];
+    var index=0;
+
+    while(index<animalData.length){
+        animallist.push(<li>{animalData[index]}</li>);
+        index++;
+    }
+
+    return <div className="Introduction">
+        <h1> {data.name} </h1>
+        <article> 
+            <p>안녕하세요 저는 {data.univ}대학교의 {data.major}과에 다니고 있습니다.</p>
+            <p>올해는 {data.age}살인데 내년엔 {data.age+1}이에요.</p>
+            <p>지금 기분은 {data.emotion}해요!</p>
+        </article>
+        <h2>좋아하는 동물</h2>
+        <ul>
+            {animallist}
+        </ul>
+    </div>;
+    }
+}
+
+export default Introduction;
