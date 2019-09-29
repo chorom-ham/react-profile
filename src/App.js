6cbbd95640b46e6994f148e28d1c97b012776592
chorom-ham <52379950+chorom-ham@users.noreply.github.com>
2019-09-29 13:37:13 +0900

    피드백 사항 수정

--- src/App.js
+++ src/App.js
@@ -5,9 +5,38 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      info:[{key:1, id:1, name:"최수민", univ:"서강", major:"컴퓨터공학", age:"25", emotion:"행복", animal:["사자", "토끼", "뱀"]},
-      {key:2, id:2, name:"이한길", univ:"홍익" , major:"법학", age:"29", emotion:"불행", animal:["펭귄"]}, 
-      {key:3, id:3, name:"김서영", univ:"이화여자", major:"사이버보안학", age:"22", emotion:"불행", animal:["웜", "트로이목마"]}]
+      info:[
+        {
+          key:1,
+          id:1, 
+          name:"최수민", 
+          university:"서강", 
+          major:"컴퓨터공학", 
+          age:25, 
+          emotion:"행복", 
+          animal:["사자", "토끼", "뱀"]
+        },
+      {
+        key:2, 
+        id:2, 
+        name:"이한길", 
+        university:"홍익" , 
+        major:"법학", 
+        age:29, 
+        emotion:"불행", 
+        animal:["펭귄"]
+      }, 
+      {
+        key:3, 
+        id:3, 
+        name:"김서영", 
+        university:"이화여자", 
+        major:"사이버보안학", 
+        age:22, 
+        emotion:"불행", 
+        animal:["웜", "트로이목마"]
+      }
+    ]
     }
   }
 
