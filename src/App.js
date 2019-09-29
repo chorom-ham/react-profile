4ccf45aafafc2cca24b408bb207e84b97847552a
chorom-ham <52379950+chorom-ham@users.noreply.github.com>
2019-09-29 16:25:11 +0900

    while, for, forEach, map 사용

--- src/App.js
+++ src/App.js
@@ -1,52 +1,98 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Introduction from './Introduction';
 
+import styled, { createGlobalStyle } from 'styled-components';
+
+const Globalstyle = createGlobalStyle`
+  html{
+    font-size = 10px
+  }
+  `
+
+const Row = styled.div`
+    display:flex;
+    flex-direction:row;
+  `
+const Introductions = ({array}) => (
+  array.map(value => <Introduction data={value}/>)
+)
+
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      info:[
+      info: [
         {
-          key:1,
-          id:1, 
-          name:"최수민", 
-          university:"서강", 
-          major:"컴퓨터공학", 
-          age:25, 
-          emotion:"행복", 
-          animal:["사자", "토끼", "뱀"]
+          key: 1,
+          id: 1,
+          name: "최수민",
+          university: "서강",
+          major: "컴퓨터공학",
+          age: 25,
+          emotion: "행복",
+          animal: ["사자", "토끼", "뱀"]
         },
-      {
-        key:2, 
-        id:2, 
-        name:"이한길", 
-        university:"홍익" , 
-        major:"법학", 
-        age:29, 
-        emotion:"불행", 
-        animal:["펭귄"]
-      }, 
-      {
-        key:3, 
-        id:3, 
-        name:"김서영", 
-        university:"이화여자", 
-        major:"사이버보안학", 
-        age:22, 
-        emotion:"불행", 
-        animal:["웜", "트로이목마"]
-      }
-    ]
+        {
+          key: 2,
+          id: 2,
+          name: "이한길",
+          university: "홍익",
+          major: "법학",
+          age: 29,
+          emotion: "불행",
+          animal: ["펭귄"]
+        },
+        {
+          key: 3,
+          id: 3,
+          name: "김서영",
+          university: "이화여자",
+          major: "사이버보안학",
+          age: 22,
+          emotion: "불행",
+          animal: ["웜", "트로이목마"]
+        }
+      ]
     }
   }
 
   render() {
-    return <div className="App"> 
-      <Introduction data={this.state.info[0]}></Introduction>
-      <Introduction data={this.state.info[1]}></Introduction>
-      <Introduction data={this.state.info[2]}></Introduction>
+    var whileInfoList = [];
+    var forInfoList = [];
+    var forEachInfoList = [];
+    
+    var whileIndex = 0;
+    while (whileIndex < this.state.info.length) {
+      whileInfoList.push(<Introduction data={this.state.info[whileIndex]}/>)
+      whileIndex++;
+    }
+
+    this.state.info.forEach(value => {
+      forEachInfoList.push(<Introduction data={value}/>)
+    })
+
+    return <div className="App">
+      <div>
+        여기에 이름이 표시됩니다.
+      </div>
+
+      <Row>
+        <Introduction data={this.state.info[0]}></Introduction>
+        <Introduction data={this.state.info[1]}></Introduction>
+        <Introduction data={this.state.info[2]}></Introduction>
+      </Row>
+      <Row>
+        {whileInfoList}
+      </Row>
+      <Row>
+        {forEachInfoList}
+      </Row>
+      <Row>
+        <Introductions array={this.state.info} />
+      </Row>
     </div>;
   }
-
 }
+
+
 export default App;
--- src/Introduction.js
+++ src/Introduction.js
@@ -1,29 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const P = styled.p`
+    font-size:1rem;
+`;
+
+const NameWrapper = styled.div`
+    font-size :2.5rem;
+    `
+
+const Wrapper = styled.div`
+    margin-right:1rem;
+    border: 1px solid grey;
+
+`
 
 class Introduction extends Component {
-  render() {
-    var data = this.props.data;
-    var animalData = this.props.data.animal;
-    var animalList = [];
-    var index=0;
+    render() {
+        var data = this.props.data;
+        var animalData = this.props.data.animal;
+        var animalList = [];
+        var index = 0;
 
-    while(index<animalData.length){
-        animalList.push(<li>{animalData[index]}</li>);
-        index++;
-    }
+        while (index < animalData.length) {
+            animalList.push(<li>{animalData[index]}</li>);
+            index++;
+        }
 
-    return <div className="Introduction">
-        <h1> {data.name} </h1>
-        <article> 
-            <p>안녕하세요 저는 {data.university}대학교의 {data.major}과에 다니고 있습니다.</p>
-            <p>올해는 {data.age}살인데 내년엔 {data.age+1}이에요.</p>
-            <p>지금 기분은 {data.emotion}해요!</p>
-        </article>
-        <h2>좋아하는 동물</h2>
-        <ul>
-            {animalList}
-        </ul>
-    </div>;
+        return <div className="Introduction">
+            <Wrapper>
+                <NameWrapper> {data.name} </NameWrapper>
+                <article>
+                    <P>안녕하세요 저는 {data.university}대학교의 {data.major}과에 다니고 있습니다.</P>
+                    <p>올해는 {data.age}살인데 내년엔 {data.age + 1}이에요.</p>
+                    <p>지금 기분은 {data.emotion}해요!</p>
+                </article>
+                <h2>좋아하는 동물</h2>
+                <ul>
+                    {animalList}
+                </ul>
+            </Wrapper>
+        </div>;
     }
 }
 
